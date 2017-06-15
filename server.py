88f134d3b4df861057cc061fbfb0c98887895ec3
Daniel Wood <daniel.wood4@extendhealth.com>
2017-06-15 16:26:21 -0600

    trying something new with the procfile

--- server.py
+++ server.py
@@ -1,14 +1,22 @@
-import http.server
-import socketserver
-import os
+# import http.server
+# import socketserver
+# import os
 
-PORT = os.environ.get('PORT') or 8080
+# PORT = os.environ.get('PORT') or 8080
 
-web_dir = os.path.join(os.path.dirname(__file__), 'JSChess')
-os.chdir(web_dir)
+# web_dir = os.path.join(os.path.dirname(__file__), 'JSChess')
+# os.chdir(web_dir)
 
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
+# Handler = http.server.SimpleHTTPRequestHandler
+# httpd = socketserver.TCPServer(("", PORT), Handler)
+# print("serving at port", PORT)
 
-httpd.serve_forever()#; except KeyboardInterrupt: pass; httpd.server_close()
+# httpd.serve_forever()#; except KeyboardInterrupt: pass; httpd.server_close()
+
+def app(environ, start_response):
+  data = b"Hello, World!\n"
+  start_response("200 OK", [
+    ("Content-Type", "text/plain"),
+    ("Content-Length", str(len(data)))
+  ])
+  return iter([data])
